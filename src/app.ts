8ef7d8ff7c8d8b108bea33f0d0034e3ca1cfbc9c
Rodrigo Brocchi <rodrigobrocchi@MacBook-Pro-de-Rodrigo-Brocchi.local>
2020-11-21 23:44:03 -0300

    Correção cors

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,8 @@ import 'dotenv/config';
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
-import 'express-async-errors';
 import cors from 'cors';
+import 'express-async-errors';
 
 import routes from './routes';
 import AppError from './errors/AppError';
@@ -19,7 +19,7 @@ require('dotenv').config({
 createConnection();
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
